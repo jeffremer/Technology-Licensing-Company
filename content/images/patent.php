d85fb44bf748cbaf42ba04fa41e9ee7839adc2db
Jeff Remer <jeff@threestarchina.com>
2011-02-22 13:32:46 -0800

    First commit

---
+++ content/images/patent.php
@@ -0,0 +1,12 @@
+<?php
+
+$page_title = 'Technology Licensing Company';
+include_once $_SERVER['DOCUMENT_ROOT'].'/includes/header.php'; // Require the HTML header.
+?>
+
+<a href="US05734862.pdf" title="Download Patent"><h3 style="text-align: center;">Download US PATENT US05734862 as an Adobe Acrobat PDF.</h3></a>
+<p align="center">
+<a href="http://www.acrobat.com" title="Acrobat Download"><img src="../images/acrobat.gif" alt="acrobat"></a>
+</p>
+
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/includes/footer.php'; // Require the HTML footer. ?>
